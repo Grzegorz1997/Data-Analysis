eb896f64268c214b909994fbcef47659ed4dd03c
Grzegorz <grzegorzjkurzeja@gmail.com>
2023-11-09 21:57:15 +0100

    loop through each team function, test postoned due to 429 response issue

--- Web Scraping Football Matches From The Premier League/data_extraction.py
+++ Web Scraping Football Matches From The Premier League/data_extraction.py
@@ -13,12 +13,15 @@ class EplDataDownload():
         full_links = [f"https://fbref.com/{link}" for link in links]
         return full_links
         
-    def get_stats_table(self):
+    def get_stats_links(self):
         url_data = requests.get(self.url_path)
         soup = BeautifulSoup(url_data.text)
         league_table = soup.select('table.stats_table')[0]
-        links = self.clean_links(league_table,"/squads/")
-        team_link = links[0] # loop for each team - next function
+        self.links = self.clean_links(league_table,"/squads/")
+        return self.links
+    
+    def get_stats_table(self,team_index):
+        team_link = self.links[team_index] # loop for each team - next function
         self.team_data = requests.get(team_link)
         self.stats_df = pd.read_html(self.team_data.text,match="Scores & Fixtures")[0]
         print(self.stats_df.head())
@@ -33,13 +36,21 @@ class EplDataDownload():
     def merge(self):
         self.merged_df = self.stats_df.merge(self.shoot_df[['Date','Sh', 'SoT','Dist', 'FK','PK', 'PKatt']], on="Date",how="inner")
         
-    def scrap_and_merge(self):
-        self.get_stats_table()
-        self.get_shooting_table()
-        self.merge()
-        return self.merged_df
-        
+    def loop_each_team(self):
+        concat_df = pd.DataFrame()
+        team_links = self.get_stats_links()
+        for team in team_links:
+            self.get_stats_table(team_links.index(team))
+            self.get_shooting_table()
+            df = self.merge()
+            self.concat_df = concat_df.append(df)
+
+    def scrap_epl_data(self):
+        self.loop_each_team()
+        return self.concat_df
+    
 if __name__ == "__main__":
     c1 = EplDataDownload("https://fbref.com/en/comps/9/2022-2023/2022-2023-Premier-League-Stats#all_league_structure")
-    df = c1.scrap_and_merge()
+    df = c1.scrap_epl_data()
     print(df.head())
+    df.to_excel(r"C:\Python Scripts\Data Analysis\Web Scraping Football Matches From The Premier League\test.xlsx")
